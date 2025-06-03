f3dd6b6a4ccf838eeeb30614adf82aa0452a5a90
Dhilan Shah <dhilanshah@Dhilans-MacBook-Air.local>
2025-06-02 23:10:35 -0400

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,31 @@
+console.log('Client side javasciript file is loaded')
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+const message1 = document.querySelector('#par1')
+const message2 = document.querySelector('#par2')
+
+
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const location = search.value
+
+    message1.textContent = 'Loading...'
+    message2.textContent = ''
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+
+        response.json().then((data)=> {
+            if(data.error){
+                message1.textContent = data.error
+            }
+            else{
+                message1.textContent = data.location
+                message2.textContent = data.forecast
+            }
+        })
+    })
+
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request')
+
+const forecast = (lat, long, callback) => {
+    const url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/'+lat+','+long+ '?unitGroup=us&key=XKZLXRURZ3SNB9WRS63C9ZHVH&contentType=json'
+
+
+    request({url, json: true}, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+         } 
+        else if (body.error) {
+            callback('Unable to find location', undefined)
+         } 
+         else {
+            callback(undefined, body.days[0].conditions + ". It is currently " + body.days[0].temp + " degress out.")
+        }
+    })
+}
+
+module.exports = forecast
